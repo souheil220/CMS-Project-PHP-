45e98805f77dbab025e7663a03d32a4eb4d23f11
souhil433 <souhil433@gmail.com>
2020-07-12 11:47:44 +0100

    Login / Logout CMS

---
+++ includes/login.php
@@ -0,0 +1,43 @@
+<?php include "db.php"?>
+<?php session_start()?>
+
+<?php 
+    if(isset($_POST["login"])){
+        $username = $_POST["username"];
+        $pass = $_POST["password"];
+
+        $username = mysqli_real_escape_string($connection,$username);
+        $pass = mysqli_real_escape_string($connection,$pass);
+
+        $query = "SELECT * FROM users WHERE username = '$username' ";
+        $select_user_query = mysqli_query($connection,$query);
+        if(!$select_user_query){
+            die('Error : '.mysqli_errno($connection));
+        }
+        
+        while ($row = mysqli_fetch_array($select_user_query)) {
+            $db_id= $row['user_id'];
+            $db_username= $row['username'];
+            $db_pass= $row['user_password'];
+            $firstName= $row['firstname'];
+            $lastName= $row['user_lastname'];
+            $role= $row['user_role'];
+        }
+        echo $lastName;
+
+        if($username !== $db_username || $pass!== $db_pass){
+            
+            header("Location: ../index.php");
+           
+        }
+        else {
+            
+            $_SESSION['username'] = $db_username;
+            $_SESSION['firstName'] = $firstName;
+            $_SESSION['lastName'] = $lastName;  
+            $_SESSION['role'] = $role;  
+
+            header("Location: ../admin");
+        }
+    }
+?>
---
+++ includes/logout.php
@@ -0,0 +1,12 @@
+<?php session_start()?>
+
+<?php 
+   
+            $_SESSION['username'] = null;
+            $_SESSION['firstName'] = null;
+            $_SESSION['lastName'] = null;  
+            $_SESSION['role'] = null;  
+
+            header("Location: ../index.php");
+      
+?>
